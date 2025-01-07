3ab840485543d40ac93b56b12e443bcdeaa3747b
Davidandr1 <157859736+Davidandr1@users.noreply.github.com>
2025-01-07 10:56:51 -0500

    Update Controller.java

--- Library/Controller.java
+++ Library/Controller.java
@@ -1,12 +1,16 @@
-package Library;
+package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.Node;
+
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Controller {
@@ -15,7 +19,16 @@ public class Controller {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
-	
+	@FXML
+	private TextField userinput;
+	@FXML
+	private TextField userinput1;
+	@FXML
+	private TextField userinput2;
+	@FXML
+	private TextField userinput3;
+	@FXML
+	private CheckBox myCheckBox;
 	public void addToLib(ActionEvent event) throws IOException{
 		Parent root = FXMLLoader.load(getClass().getResource("AddToLib.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -52,7 +65,17 @@ public class Controller {
 	}
 	
 	public void saveInstrument(ActionEvent event) throws IOException{
-		//Save Instruments to a file
+		String file = "Library.txt";
+		String checkBox;
+		FileWriter fileWriter = new FileWriter(file,true);
+		String str1 = userinput1.getText();
+		String str2 = userinput2.getText();
+		String str3 = userinput3.getText();
+		if(myCheckBox.isSelected()) { checkBox = "Yes"; }
+		else { checkBox="no"; }
+		fileWriter.write(str1+"#"+str2+"#"+str3+"#"+checkBox+"\n");
+		fileWriter.flush();
+		fileWriter.close();
 	}
 	
 }
